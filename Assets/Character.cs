9423efc58c5a9ac2d8b31ecc30472a5bceb041a4
martinUbilla <martin.ubilla@alumnos.ucn.cl>
2025-05-09 21:34:22 -0400

    menu sin decorar

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem.Processors;
 
 public class Character : MonoBehaviour
 {
     public int maxHp = 1000;
     public int currentHp = 1000;
     [SerializeField] StatusBar hpBar;
+    private bool isDead;
+
     public void TakeDamage(int damage)
     {
+        if (isDead == true)
+        {
+            return;
+        }
         currentHp -= damage;
         if(currentHp < 1)
         {
-            Destroy(gameObject);
+            GetComponent<CharacterGameOver>().GameOver();
+            isDead = true;
         }
         hpBar.SetState(currentHp, maxHp);
     }
